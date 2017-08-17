72b1b3e0efa3661b70996fedfcba8b5584814eac
JamesCoy1229 <30748815+JamesCoy1229@users.noreply.github.com>
2017-08-17 11:09:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 AOS.init({
   duration: 1200,
 })
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    loop: false,
+    autoplay: false,
+    path: 'data.json',
+})
